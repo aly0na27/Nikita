0e4216c9b9049af3560b765f7f737f804bc150bb
aly0na27 <alenakravchenko1910@gmail.com>
2023-08-16 20:07:24 +0300

    localStorage

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-let counter = 0;
+var counter;
 
+if (!localStorage.getItem("counter")) {
+    localStorage.setItem("counter", (0).toString());
+    // console.log(9);
+} else {
+  counter = parseInt(localStorage.getItem("counter"));
+}
 const counterValue = document.getElementById("input_text");
 const incrementCounter = document.getElementById("plus");
 incrementCounter.addEventListener("click", () => {
     counter++;
     counterValue.innerHTML = counter;
 })
+localStorage.setItem("counter", counter.toString());
